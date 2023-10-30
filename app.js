9c017d5a069e99969a6a5baaaa716539a6f4dfcf
sourabh.kumar3312@gmail.com <sourabh.kumar3312@gmail.com>
2023-10-30 21:52:04 +0530

    service worker impl

---
+++ app.js
@@ -0,0 +1,5 @@
+const webpush = require('web-push');
+console.log(webpush.generateVAPIDKeys());
+
+const publicKey = "BGkpGWofgj0KwjCwZVraUGScgUapDkMRrYwd5cDOVKiBtvQq_6zcoRNk_FF5j4bJpqswlNcy0oD-7-Wu4AW5LPE";
+const privateKey = "FgY3IKDnXAbvRTTFjvFCqqN2qtwfiR_KWcc23RpFi0U";
